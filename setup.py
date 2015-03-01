bd311cc424ebbe8a58cebe25b65dc188bfdd389d
gescheit <gescheit@yandex-team.ru>
2015-03-01 21:10:04 +0300

    update

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@ from Cython.Distutils import build_ext
 
 classifiers = [
     'Development Status :: 4 - Beta',
-    'Environment :: Console',
     'Operating System :: POSIX :: Linux',
     'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
     'Programming Language :: Python :: 3 :: Only',
     'Topic :: System :: Networking :: Monitoring',
-],
-
+]
 cmdclass = {}
 
 try:
@@ -26,19 +24,31 @@ else:
 ext = '.pyx' if USE_CYTHON else '.c'
 extensions = [Extension("fastsnmp/snmp_parser", ["fastsnmp/snmp_parser" + ext])]
 
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 if __name__ == "__main__":
     if USE_CYTHON:
         from Cython.Build import cythonize
         extensions = cythonize(extensions)
-
     setup(name='fastsnmp',
           ext_modules=extensions,
           version=__version__,
           author="Aleksandr Balezin",
           packages=['fastsnmp'],
-          license='LICENSE',
+          package_data = {'lib': ['*.pyx', '*.c', 'README.md', 'examples/*.py']},
+          license='GPLv2',
           url='https://github.com/gescheit/fastsnmp',
           author_email='gescheit12@gmail.com',
           classifiers=classifiers,
           platforms=['Linux'],
+          long_description=read_md('README.md'),
+          keywords="SNMP poller parser library coder decoder",
+          description="SNMP poller oriented to poll bunch of hosts in short time. Package include poller and SNMP library",
+          requires = ["Cython"],
           )
