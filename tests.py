d19b9a7aa645824c309a5216ef63874bf1c7ceb8
gescheit <gescheit@yandex-team.ru>
2015-03-22 01:43:58 +0300

    fix func name

--- tests.py
+++ tests.py
@@ -9,11 +9,11 @@ class TestSnmpParser(unittest.TestCase):
         self.target_oid = "1.3.6.1.2.1.2.2.1.2"
         self.target_oid_encoded = bytes([43, 6, 1, 2, 1, 2, 2, 1, 2])
 
-    def testOIDCoder(self):
+    def test_oid_coder(self):
         oid_encoded = snmp_parser.objectid_encode(self.target_oid)
         self.assertEqual(self.target_oid_encoded, oid_encoded)
 
-    def testOIDDecoder(self):
+    def test_oid_decoder(self):
         oid_decoded = snmp_parser.objectid_decode(self.target_oid_encoded)
         self.assertEqual(self.target_oid, oid_decoded)
 
